aaf530932af5570ffed96f7deb23d692d491d85c
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2020-11-29 10:55:36 +0530

    Update 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,27 @@
-import React, { Component, useState } from "react";
-import {
-  Link,
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  useLocation
-} from "react-router-dom";
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "../styles/App.css";
+import LocationDisplay from "./locationDisplay";
+import Home from "./home";
+import About from "./about";
+import Other from "./other";
 
-function Home() {
-  return <div>You are home.</div>;
-}
-function About() {
-  return <div>You are on the about page.</div>;
-}
-function Other() {
-  return <div>No match</div>;
-}
-function LocationDisplay() {
-  const location = useLocation();
-  return <div data-testid="location-display">{location.pathname}</div>;
-}
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <Router>
-          <div id="main">
-            <a href="/">Home</a>
-            <br />
-            <a href="/about">About</a>
-            <LocationDisplay />
-            <Switch>
-              <Route exact path="/about" component={About} />
-              <Route exact path="/" component={Home} />
-              <Route path="/" component={Other} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <div id="main">
+          <a href="/">Home</a>
+          <br />
+          <a href="/about">About</a>
+          <LocationDisplay />
+          <Switch>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route path="/" component={Other} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <div>You are on the about page</div>;
+}
